58755e8fc2afa81738f120d12a857457f0f3ac09
Coutinho de Macedo <jsoh@gft.com>
2022-04-28 12:15:15 -0300

    Project Created

---
+++ web-transactional/src/main/java/com/gft/WebTransactionalApplication.java
@@ -0,0 +1,13 @@
+package com.gft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebTransactionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebTransactionalApplication.class, args);
+	}
+
+}
